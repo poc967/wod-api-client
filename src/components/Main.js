c7372f336d2e7f8151c540da8792649a13df0e3d
Patrick O'Connor <poc.967@gmail.com>
2022-08-29 22:24:12 -0400

    update main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,10 +18,62 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
+const Title = styled.span`
+  font-size: 1.125rem;
+`;
+
+const WorkoutBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 95vw;
+  margin: 0 auto;
+  border: solid black 2px;
+  border-radius: 0.5rem;
+  margin-top: 15px;
+`;
+
+let mockWorkOutData = {
+  title: 'test title',
+  date: null,
+  work_outs: [
+    {
+      id: '62f7196b980d371bb834fc81',
+      description: 'AMRAP 10',
+      is_deleted: false,
+      movements: [
+        {
+          movement: {
+            id: '62f7131f441f080786bcf621',
+            name: 'wall balls',
+            is_deleted: false,
+            created: 'Fri Aug 12 22:57:34 2022',
+          },
+          repititions: '20',
+          sets: null,
+          weight: '20/14',
+          notes: null,
+        },
+      ],
+      notes: 'unbroken',
+      created: 'Fri Aug 12 23:24:25 2022',
+    },
+  ],
+};
 class Main extends Component {
   render() {
     return (
       <Wrapper>
+        <Title>Aug 18, 2022</Title>
+        {mockWorkOutData.work_outs.map((work_out) => (
+          <WorkoutBox>
+            {work_out.description}
+            <ul style={{ listStyleType: 'none' }}>
+              {work_out.movements.map((movement) => (
+                <li>{`${movement.repititions} ${movement.movement.name} (${movement.weight})`}</li>
+              ))}
+            </ul>
+          </WorkoutBox>
+        ))}
         <ButtonContainer>
           <Button1 text="New Workout" />
           <Button1 text="Whiteboard" />
